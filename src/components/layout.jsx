33bcd1c96ef8f3a4388eaf87e145ae943b4da4af
masroorali <masroor.ali@linentech.net>
2024-04-30 22:17:07 +0500

    fixed build issue

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from "react";
 import Header from "./header";
-import Footer from "./footer";
 
 const Layout = (props) => {
   return (
     <Fragment>
       <Header />
       {props.children}
-      {/* <Footer /> */}
     </Fragment>
   );
 };
--- src/pages/filter.jsx
+++ src/pages/filter.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import CardsGrid from "../components/cards_grid";
-import CustomTabs from "../components/custom_tabs";
 
 const Filter = () => {
   return (
