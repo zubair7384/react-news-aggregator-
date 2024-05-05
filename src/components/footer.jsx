b403f741d7760070f99f3ab6891df4044a059513
masroorali <masroor.ali@linentech.net>
2024-05-05 16:06:05 +0500

    added gnews

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { Link, useLocation } from "react-router-dom";
-import { Divider } from "antd";
 
 const Footer = () => {
   const location = useLocation();
